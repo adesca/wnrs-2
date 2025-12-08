9973bf2ea402ba61d2b40a939ccdb738a72c7210
adesca <adeolubabatunde@gmail.com>
2025-12-08 14:55:56 -0600

    wild card support

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,9 @@ interface CardProps {
 
 export function Card(props: CardProps) {
     const {text, classes = []} = props;
+    const cardClass = text.toLowerCase().startsWith('wildcard') ? 'wild' : ''
 
-    return <div className={`card has-text-weight-bold ${classes.join(' ')}`}>
+    return <div className={`card ${cardClass} has-text-weight-bold ${classes.join(' ')}`}>
         <div className="card-content">
             <div className="content">
                 {text}
--- src/components/PlayingArea.tsx
+++ src/components/PlayingArea.tsx
@@ -15,20 +15,31 @@ export function PlayingArea() {
     } = useDeckContext();
     const currLevelMeta = gameState[`${currLevel}_meta`]
 
-    return <div className={'playing-area' +
-        ' is-flex is-flex-direction-column is-flex-wrap-wrap is-align-content-center'}>
-        <div>Deck: {gameState.name}, <span className={'is-capitalized'}>{currLevelMeta.levelName}</span></div>
-        <div className={'displayed-cards'}>
-            <Card text={currCard}/>
+    return <>
+        <div className={'playing-area' +
+            ' is-flex is-flex-direction-column is-flex-wrap-wrap is-align-content-center'}>
+
+            <div>Deck: {gameState.name}, <span className={'is-capitalized'}>{currLevelMeta.levelName}</span></div>
+            <div className={'displayed-cards'}>
+                <Card text={currCard}/>
+            </div>
+
+
+            <div className={'mx-auto'}>{currLevelMeta.cardsViewed + 1}/{currLevelMeta.totalDeckSize}</div>
+
+            <span className={'mx-auto mt-5 buttons'}>
+                <Button onClick={handleNext}>Next Card</Button>
+                <Button popoverTarget={'card-list'}>Cheat</Button>
+            </span>
         </div>
-
-
-        <div className={'mx-auto'}>{currLevelMeta.cardsViewed + 1}/{currLevelMeta.totalDeckSize}</div>
-
-        <span className={'mx-auto mt-5'}>
-            <Button onClick={handleNext}>Next Card</Button>
-        </span>
-    </div>
+        <dialog popover={'auto'} id={'card-list'}>
+            <span className={'card-history'}>
+                <ul>
+                   {gameState[currLevel].map(card => <Card key={card} text={card} classes={['mx-auto']} />)}
+                </ul>
+            </span>
+        </dialog>
+    </>
 }
 
 
